130a9c25fdc9a28942c4a9abe00ec5ab8782caa3
shanelevites <srlevit94@gmail.com>
2023-03-15 17:27:47 -0700

    update Navigation.js:

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Navigation from "./components/Navigation";
+// import Project from "./components/Project";
+
+// function App() {
+//     return (
+//       <div>
+//         <Header />
+//         <Header />
+//         <Section />
+//       </div>
+//     );
+//   }
+
 const App = () => <PortfolioContainer />;
 
 export default App;
