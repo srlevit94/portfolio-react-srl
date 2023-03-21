03c1347d5eda23598df4c273b64efb03a736e4b5
shanelevites <srlevit94@gmail.com>
2023-03-20 18:32:34 -0700

    uploaded Resume and made download button

--- README.md
+++ README.md
@@ -30,6 +30,8 @@
 
 - WHEN I am presented with the Resume section
 - THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
+
+
 - WHEN I view the footer
 - THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter)
 
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import resumePdf from '../../assets/LevitesResume.pdf';
+
+
+const handleResumeClick = () => {
+  window.open(resumePdf);
+}
+
+const styles = {
+  resume: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    padding: "40px",
+  }
+}
 
 export default function Resume() {
   return (
-    <div>
-      <h2>Resume!</h2>
+    <div 
+    
+    style={styles.resume}
+    onClick={handleResumeClick}>
+      <button className="btn btn-outline-primary btn-lg btn-block">Click to Download Resume</button>
     </div>
   );
 }
